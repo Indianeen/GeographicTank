8cee2b1143606cc13634530f8e229b13d11d0a2e
Indianeen <axel.lundberg@elev.ga.ntig.se>
2022-12-09 09:09:43 +0100

    Progress?

--- my-app/main.js
+++ my-app/main.js
@@ -1,13 +1,23 @@
 import './style.css';
-import {Map, View} from 'ol';
 import TileLayer from 'ol/layer/Tile';
 import OSM from 'ol/source/OSM';
+import GeoJSON from 'ol/format/GeoJSON';
+import Map from 'ol/Map';
+import VectorLayer from 'ol/layer/Vector';
+import VectorSource from 'ol/source/Vector';
+import View from 'ol/View';
 
 const map = new Map({
   target: 'map',
   layers: [
     new TileLayer({
       source: new OSM()
+    }),
+    new VectorLayer({
+      source: new VectorSource({
+        format: new GeoJSON(),
+        url: './data/countries.json',
+      })
     })
   ],
   view: new View({
