dc4dc9feeb7253c40299569d0e3e9170e3f2a8a9
Indianeen <axel.lundberg@elev.ga.ntig.se>
2022-12-13 14:52:50 +0100

    Many changes

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
-import OSM from 'ol/source/OSM';
+import './style.css';
+import OSM from 'ol/source/OSM.js';
 import TileLayer from 'ol/layer/Tile';
-import {Map, View} from 'ol';
+import Map from 'ol/Map.js';
+import View from 'ol/View.js';
+import TileLayer from 'ol/layer/Tile.js';
 import {fromLonLat} from 'ol/proj';
 import GeoJSON from 'ol/format/GeoJSON';
 import VectorLayer from 'ol/layer/Vector';
@@ -12,7 +15,12 @@ import Link from 'ol/interaction/Link';
 const source = new VectorSource();
 
 const map = new Map({
-  target: 'map-container',
+  target: 'map',
+  layers: [
+    new TileLayer({
+      source: new OSM(),
+    }),
+  ],
   view: new View({
     center: fromLonLat([0, 0]),
     zoom: 2,
